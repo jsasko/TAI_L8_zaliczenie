8e9c09f0e57b83322764835e295cab97d1190179
Kuba <kuba.sasko@gmail.com>
2018-12-13 19:32:24 +0100

    Init lab 8

---
+++ src/app/components/blog-details-page/blog-details-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {BlogService} from '../../services/blog.service';
+
+@Component({
+  selector: 'app-blog-details-page',
+  templateUrl: './blog-details-page.component.html',
+  styleUrls: ['./blog-details-page.component.css']
+})
+export class BlogDetailsPageComponent implements OnInit {
+
+  post: any;
+  constructor(private route: ActivatedRoute, private blogService: BlogService) { }
+
+  ngOnInit() {
+      this.blogService.getPost(this.route.params['id']).subscribe( (data) => {
+        this.post = data;
+      });
+  }
+
+}
