5a0ea162fefcc632dc5ec895abef2bba03a9bf29
jsasko <28355@student.pwsztar.edu.pl>
2018-12-13 19:38:02 +0100

    Init lab 8

--- api/app/middleware/auth.js
+++ api/app/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const auth = (req, res, next) =>{
   const token = req.header('x-auth-token');
   if (!token) return res.status(401).send('Access denied. No token provided.');
-
+//
   try {
     const decoded = jwt.verify(token, 'secret');
     req.user = decoded;
